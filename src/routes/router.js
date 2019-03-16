ea47828485898a2ca70c8d291f63ab44b51d6ad5
Kate <katarinamadness@mail.ru>
2019-03-16 10:07:55 +0200

    initial commit

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,17 +1,20 @@
+const express = require('express');
 const mainRoute = require ('./main/main');
 const signUpRoute = require ('./users/sign-up-route');
 const getAllProducts = require ('./products/products');
 const getProduct = require ('./products/products');
 const getCategory = require ('./products/products');
 
+const apiRoutes = express.Router();
 
-const router = {
-  '/signup': signUpRoute,
-  '/products': getAllProducts,
-    '/products/get': getProduct,
-    '/products/getcategory': getCategory,
-    // /products/get?productId=string
-  default: mainRoute
-};
+apiRoutes
+    .get('/', mainRoute)
+    .get('/signup', signUpRoute)
+    .get('/products', getAllProducts)
+    .get('/products/get', getProduct,)
+    .get('/products/getcategory', getCategory)
 
-module.exports = router;
+
+
+module.exports = apiRoutes;
+
--- src/server.js
+++ src/server.js
@@ -1,25 +1,29 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = require('./modules/app');
 const morgan = require('morgan');
 const router = require('./routes/router');
 
-const logger = morgan('combined');
+const errorHandler = (req, res, next)  => {
+    res.status(500).send('No such page');
+    next();
+};
+
+const staticPath = path.join(__dirname, '..', 'assets');
 
 const startServer = port => {
+    app
+        .use(bodyParser.urlencoded({ extended: false }))
+        .use(bodyParser.json())
+        .use(morgan('dev'))
+        .use(express.static(staticPath))
+        .use('/', router)
+        .use(errorHandler);
 
-  const server = http.createServer((request, response) => {
+    app.listen(port);
 
-    // Get route from the request
-    const parsedUrl = url.parse(request.url);
-    //console.log(parsedUrl);
-    // Get router function
-    const func = router[parsedUrl.pathname] || router.default;
-
-    logger(request, response, () => func(request, response));
-  });
-
-  server.listen(port);
+    console.log('Server was started at http://localhost:' + port);
 };
 
 module.exports = startServer;
